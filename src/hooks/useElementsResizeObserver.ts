9e46df429f63fdb770d2e22906cd291e513bf916
ishioka <72128512+mic9-ishioka@users.noreply.github.com>
2023-06-27 08:51:17 +0900

    fix : refactor

--- src/hooks/useElementsResizeObserver.ts
+++ src/hooks/useElementsResizeObserver.ts
@@ -1,14 +1,12 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 
 export const useElementsResizeObserver = () => {
-  const [width, setWidth] = useState(0)
   const ref = useRef(null)
-
+  const [width, setWidth] = useState(0)
   const handleResize = useCallback((entries: ResizeObserverEntry[]) => {
     const width = entries[0].contentRect.width
     setWidth(Math.floor(width))
   }, [])
-
   useEffect(() => {
     const resizeObserver = new ResizeObserver((entries) => {
       handleResize(entries)
